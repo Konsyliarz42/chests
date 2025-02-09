1c0d2458133ed9dfea816bed719db512909089c3
Tomasz Kordiak <dotomika@gmail.com>
2025-02-09 15:22:52 +0100

    First release (#1)

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+
+## [1.0.0] - 09.02.2025
+
+### Added
+
+- Normal chest
+- Normal double chest
+- Trapped chest
+- Double trapped chest
+
+[Unreleased]: https://github.com/Konsyliarz42/chests/compare/1.0.0...master
+[1.0.0]: https://github.com/Konsyliarz42/chests/tree/1.0.0
---
+++ make_zip.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from typing import Final
+from zipfile import ZipFile
+
+PACKAGE_NAME: Final = "Chests"
+REQUIRED_PATHS: Final = [
+    Path("assets"),
+    Path("pack.png"),
+    Path("pack.mcmeta"),
+]
+IGNORED_SUFFIXES: Final = [
+    ".pfi",
+]
+
+
+def check_required_paths(paths: list[Path]) -> None:
+    for path in paths:
+        if not path.exists():
+            raise FileNotFoundError(f"Required path: `{path.name}` not found.")
+
+
+def add_file_to_zip(zip: ZipFile, file: Path) -> None:
+    if file.suffix not in IGNORED_SUFFIXES:
+        zip.write(file)
+
+
+def create_zip_file(zip_name: str, zip_content: list[Path]) -> None:
+    with ZipFile(zip_name, "w") as zip_file:
+        for target in zip_content:
+            if target.is_dir():
+                [add_file_to_zip(zip_file, file) for file in target.glob("**/*") if file.is_file()]
+            else:
+                add_file_to_zip(zip_file, target)
+
+
+if __name__ == "__main__":
+    print("Create a new zip file")
+    check_required_paths(REQUIRED_PATHS)
+
+    version = input("Version: ")
+    zip_name = f"{PACKAGE_NAME}-{version}.zip"
+    create_zip_file(zip_name, REQUIRED_PATHS)
+    print("Zip created:", zip_name)
